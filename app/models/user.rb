695df994b19ea00cb04d12b3b1a185351c94ca9e
popo <arnaud.polette@gmail.com>
2016-04-18 17:51:17 +0200

    some exercices

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
 
-    before_save { self.email = email.downcase }
+    before_save { self.email = email.downcase! }
 
     validates :name,  presence: true,
                       length: { maximum: 50 },
                       uniqueness: { case_sensitive: false }
 
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true,
                       length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
@@ -15,4 +15,5 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :password, presence: true,
                          length: { minimum: 6 }
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -64,4 +64,11 @@ RSpec.describe User, type: :model do
     !!popo.authenticate('foobar')
   end
 
+  it "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
 end
